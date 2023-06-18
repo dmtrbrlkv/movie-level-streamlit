feb142b304c65e2448326abf12564fadeeda2cf6
Dmitry Burlakov <dburlakov@mtsbank.ru>
2023-06-18 18:40:07 +0300

    Возможность ввести текст

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def get_subs_text(srt_file):
 
 
 def make_predict_proba(subs_text):
-    df = pd.DataFrame({'subs': [subs]}, index=[srt_file.name])
+    df = pd.DataFrame({'subs': [subs_text]})
     res = {}
     predict_proba = pipeline.predict_proba(df)
     for i in range(len(pipeline.classes_)):
@@ -67,29 +67,42 @@ def make_predict_proba(subs_text):
     return res
 
 
+def process_text(subs_text):
+    with st.spinner(''):
+        predict_proba = make_predict_proba(subs_text)
+        predict_proba = pd.DataFrame({'proba': predict_proba.values()}, index=predict_proba.keys())
+        predict = predict_proba['proba'].idxmax()
+        fig = plt.figure(figsize=(1, 1))
+        ax = sns.barplot(x=predict_proba.index, y=predict_proba['proba'])
+        ax.set(title='Вероятность уровня')
+        ax.set(ylabel='')
+
+    st.success(f'Уровень английского: {predict}')
+    st.pyplot(fig)
+
 st.set_page_config(page_title='Уровень английского в фильме')
 st.title('Уровень английского в фильме')
 
-st.header('Файл с субтитрами')
-srt_files = st.file_uploader('Файл с субтитрами', 'srt', label_visibility='hidden', accept_multiple_files=True)
+types = ['Текст', 'Файл субтитров']
+st.header('Входящие данные')
+input_type = st.radio('', types, horizontal=True)
+
+st.header(input_type)
+if input_type == types[0]:
+    text = st.text_area('')
+else:
+    srt_files = st.file_uploader('Файл с субтитрами', 'srt', label_visibility='hidden', accept_multiple_files=True)
 
 predict_btn = st.button('Узнать уровень английского', use_container_width=True)
 
 if predict_btn:
-    for srt_file in srt_files:
-        st.header(srt_file.name)
-        subs = get_subs_text(srt_file)
-        if subs is None:
-            st.warning('Не удалось прочитать файл с субтитрами')
-        else:
-            with st.spinner(''):
-                predict_proba = make_predict_proba(subs)
-                predict_proba = pd.DataFrame({'proba': predict_proba.values()}, index=predict_proba.keys())
-                predict = predict_proba['proba'].idxmax()
-                fig = plt.figure(figsize=(1, 1))
-                ax = sns.barplot(x=predict_proba.index, y=predict_proba['proba'])
-                ax.set(title='Вероятность уровня')
-                ax.set(ylabel='')
-
-            st.success(f'Уровень английского: {predict}')
-            st.pyplot(fig)
+    if input_type == types[0]:
+        process_text(text)
+    else:
+        for srt_file in srt_files:
+            st.header(srt_file.name)
+            subs = get_subs_text(srt_file)
+            if subs is None:
+                st.warning('Не удалось прочитать файл с субтитрами')
+            else:
+                process_text(subs)
